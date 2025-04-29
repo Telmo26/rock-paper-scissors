80d398adefe9144961500a4f36802514d3fec4ea
Tadros Eliott <eliott.tadros@student-cs.fr>
2025-04-29 14:59:56 +0200

    Implemented computer choice

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello World")
+// console.log("Hello World")
+
+function getComputerChoice() {
+    let randomChoice = Math.floor(Math.random() * 3);
+    switch(randomChoice) {
+        case 0: 
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors"
+        default:
+            return "error"
+    }   
+}
+
+console.log(getComputerChoice())
