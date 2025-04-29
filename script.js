bd624dfdd6018c20d5a49ebcb13951994c4b6846
Tadros Eliott <eliott.tadros@student-cs.fr>
2025-04-29 15:12:40 +0200

    Implemented a round of play

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 // console.log("Hello World")
 
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
-    let randomChoice = Math.floor(Math.random() * 3);
-    switch(randomChoice) {
+    let computerChoice = Math.floor(Math.random() * 3);
+    switch(computerChoice) {
         case 0: 
             return "rock";
         case 1:
@@ -15,9 +18,61 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    let humanChoice = prompt("Rock, paper, scissors ?").toLowerCase();
+    let humanChoice = prompt("Rock, paper, scissors ?");
     return humanChoice
 }
 
-console.log(getComputerChoice())
-console.log(getHumanChoice())
+function playRound(humanChoice, computerChoice) {
+    let humanChoice = humanChoice.toLowerCase();
+
+    switch(humanChoice) {
+        case "rock":
+            switch(computerChoice) {
+                case "rock":
+                    console.log("You tie!")
+                    break;
+                case "paper":
+                    console.log("You lose! Paper beats rock.")
+                    computerScore++;
+                    break;
+                case "scissors":
+                    console.log("You win! Rock beats scissors.")
+                    humanScore++;
+                    break;
+            }
+        case "paper":
+            switch(computerChoice) {
+                case "rock":
+                    console.log("You win! Paper beats rock.")
+                    humanScore++;
+                    break;
+                case "paper":
+                    console.log("You tie!")
+                    break;
+                case "scissors":
+                    console.log("You lose! Scissors beats paper.")
+                    computerScore++;
+                    break;
+            }
+        case "scissors":
+            switch(computerChoice) {
+                case "rock":
+                    console.log("You lose! Rock beats scissors.")
+                    computerScore++;
+                    break;
+                case "paper":
+                    console.log("You win! Scissors beats paper.")
+                    humanScore++;
+                    break;
+                case "scissors":
+                    console.log("You tie!")
+                    break;
+            }
+    }
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, humanSelection);
